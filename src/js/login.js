db51f165dfb14391d0a41a5694077f9805a7f6ee
David Levy Christianson <dlev0002@student.monash.edu>
2020-09-23 15:46:14 +0700

    Added sign out, authentication, track & display user across landing page

--- src/js/login.js
+++ src/js/login.js
@@ -18,7 +18,7 @@
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: 'login_successful.html',
+        signInSuccessUrl: 'welcome.html',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
