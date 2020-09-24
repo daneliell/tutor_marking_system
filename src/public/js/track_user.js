9b31b492f125b64eb8ebb21c33f491a291bd55c9
cwon0040 <cwon0040@student.monash.edu>
2020-09-24 14:14:33 +0800

    Merging hosting to master

--- src/public/js/track_user.js
+++ src/public/js/track_user.js
@@ -9,7 +9,12 @@ var mainApp = {};
             // User is signed in.
             userId = user;
             const displayUserUI = document.getElementById("displayUserUI")
+            const displayNameUI = document.getElementById("displayNameUI")
+            const displayEmailUI = document.getElementById("displayEmailUI")
             displayUserUI.textContent = String(user.displayName)
+            displayNameUI.textContent = String(user.displayName)
+            displayEmailUI.textContent = String(user.email)
+
         }else{
             userId = null;
             // Redirect back to Login page
