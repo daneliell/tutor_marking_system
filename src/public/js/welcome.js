f05019f8858a001ffbc593837822ea0e97a08f8d
David Levy Christianson <dlev0002@student.monash.edu>
2020-09-28 14:57:00 +0700

    New User will now be added to the firestore 'students' collection

--- src/public/js/welcome.js
+++ src/public/js/welcome.js
@@ -3,17 +3,48 @@ var mainApp = {};
 
 (function(){
     var firebase = app_fireBase;
-    var userId = null;
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            userId = user;
-            const displayUserUI = document.getElementById("displayUserUI")
-            const displayNameUI = document.getElementById("displayNameUI")
-            const displayEmailUI = document.getElementById("displayEmailUI")
+            const 
+                displayUserUI = document.getElementById("displayUserUI"),
+                displayNameUI = document.getElementById("displayNameUI"),
+                displayEmailUI = document.getElementById("displayEmailUI"),
+
+                userName = user.displayName,
+                userEmail = user.email,
+                docID = userEmail.substr(0, 8),
+                db = firebase.firestore(),
+                studentRef = db.collection("students").doc(docID)
+
             displayUserUI.textContent = String(user.displayName)
             displayNameUI.textContent = String(user.displayName)
             displayEmailUI.textContent = String(user.email)
+
+            studentRef.get()
+                .then((docSnapShot) => {
+                    if (docSnapShot.exists) {
+                        console.log("document already exists!")
+                        //do nothing
+                    } else {
+                        // create new data
+                        studentRef.set({
+                            id: docID,
+                            name: userName,
+                            email: userEmail,
+                            status: "student",
+                            projects: []
+                        }, {merge: true})
+                        .then(function() {
+                            console.log("Document successfully written!");
+                        })
+                        .catch(function(error) {
+                            console.error("Error writing document: ", error);
+                        });
+                    }
+                })
+
+
 
         }else{
             userId = null;
