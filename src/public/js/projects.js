f433283666970dcc424066ec80bca7716e662e46
Dan <dyue0003@student.monash.edu>
2020-10-03 23:09:01 +0800

    Updated HTML for add_project.html and projects.html For projects.html: Standardized font Centered projects and added title Added functionality to display projects for current student user Updated delete buttons
    For add_project.html:
    Standardized font
    Updated input fields

--- src/public/js/projects.js
+++ src/public/js/projects.js
@@ -1,108 +1,151 @@
 projects_list = [];
-function test(){
-
-    var user = firebase.auth().currentUser;
-
-    if (user != null) {
-      name = user.displayName; //get display name
-      email = user.email; //get email
-      uid = user.uid;  // The user's ID, unique to the Firebase. Try not to use this, use email instead
-      console.log(name + "," + email + "," + uid);
-    }
-}
+var firestore = firebase.firestore();
 
 window.onload = function(){
-  var firestore = firebase.firestore();
+  // Check if a user is logged in
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // If logged in, get the user's ID
+      let id = user.email.substring(0,8);
 
-  let i = 0;
-  firestore.collection("projects").get().then(function(querySnapshot){
-    querySnapshot.forEach(function(doc){
-      projects_list.push(doc.data());
-    })
-  }).then(function(){
-    if (projects_list.length == 0){
-      let help_text = document.createTextNode("You have no active projects.");
-      let title = document.createElement("h5");
-      title.appendChild(help_text);
-      area.appendChild(title);
+      firestore.doc("students/" + id).get().then(function(doc){
+        if (doc.data().status == "student")
+        {
+          /*let student_projects = [];
+          for (let i = 0; i < doc.data().projects.length; i++)
+          {
+            student_projects.push(doc.data().projects[i]);
+          }
+          firestore.collection("projects").get().then(function(querySnapshot){
+            querySnapshot.forEach(function(project){
+              for (let j = 0; j < student_projects.length; j++)
+              {
+                if ((project.data().title + project.data().unit) == student_projects[j])
+                {
+                  projects_list.push(project.data());
+                }
+              }
+            })
+          }).then(function(){
+            generate_html();
+          });*/
+          for (let i = 0; i < doc.data().projects.length; i++)
+          {
+            // Pushes every project student is involved into array
+            firestore.doc("projects/" + doc.data().projects[i]).get().then(function(project) {
+              projects_list.push(project.data());
+            }).then(function(){
+              // If statement to only allow function to be called once after
+              // all projects have been added
+              if (i == (doc.data().projects.length-1))
+              {
+                generate_html();
+              }
+            });
+          }
+        }
+        else
+        {
+          // Pushes all available projects into projects_list
+          firestore.collection("projects").get().then(function(querySnapshot){
+            querySnapshot.forEach(function(doc){
+              projects_list.push(doc.data());
+            })
+          }).then(function(){
+            generate_html();
+          });
+        }
+      });
     }
-    else
-    {
-      let list = document.createElement("ul");
-      list.setAttribute("class","mdc-list mdc-list--two-line");
-      let grid = document.createElement("div");
-      grid.setAttribute("class","mdc-layout-grid")
-      let grid_inner = document.createElement("div");
-      grid_inner.setAttribute("class", "mdc-layout-grid__inner");
-
-      grid.appendChild(grid_inner);
-      list.appendChild(grid);
-      area.appendChild(list);
-
-      for (let i = 0; i < projects_list.length; i++){
-        let project_name = document.createTextNode(projects_list[i].title);
-        let primary_text = document.createElement("span");
-        primary_text.setAttribute("class","mdc-list-item__primary-text");
-        primary_text.appendChild(project_name);
-
-        let due_date = document.createTextNode("Due Date: " + projects_list[i].due_date);
-        let secondary_text = document.createElement("span");
-        secondary_text.setAttribute("class","mdc-list-item__secondary-text");
-        secondary_text.appendChild(due_date);
-
-        let text = document.createElement("span");
-        text.setAttribute("class","mdc-list-item__text");
-        text.append(primary_text);
-        text.append(secondary_text);
-
-        let ripple = document.createElement("span");
-        ripple.setAttribute("class", "mdc-list-item__ripple")
-
-        let ripple_button = document.createElement("div");
-        ripple_button.setAttribute("class", "mdc-button__ripple")
-
-        let icon = document.createElement("i");
-        icon.setAttribute("class", "material-icons");
-        let del_icon = document.createTextNode("delete");
-        icon.appendChild(del_icon);
-
-        let button = document.createElement("button");
-        button.setAttribute("class", "mdc-button mdc-button--raised")
-        button.setAttribute("type", "button");
-        button.addEventListener("click", function(){
-          del_item(i);
-        });
-        button.appendChild(ripple_button);
-        button.appendChild(icon);
-
-        let lecturer = document.createElement("div");
-        lecturer.setAttribute("class", "lecturer-only admin-only");
-        lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
-        lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
-        lecturer.appendChild(button);
-
-        let link = document.createElement("a");
-        link.setAttribute("href", "details.html?project="+encodeURIComponent(projects_list[i].title+projects_list[i].unit));
-        link.setAttribute("style", "text-decoration:none;color: black;")
-        link.appendChild(text);
-
-        let new_item = document.createElement("li");
-        new_item.setAttribute("class","mdc-list-item")
-        new_item.appendChild(link);
-        new_item.appendChild(ripple);
-        new_item.appendChild(lecturer);
-
-        let cell = document.createElement("div");
-        cell.setAttribute("class", "mdc-layout-grid__cell");
-        cell.setAttribute("id", "item"+i);
-        cell.appendChild(new_item);
-
-        grid_inner.appendChild(cell);
-      }
+    else {
+      console.log("No user is signed in.");
     }
   });
-};
+}
 
+
+function generate_html()
+{
+  if (projects_list.length == 0){
+    let help_text = document.createTextNode("You have no active projects.");
+    let title = document.createElement("h5");
+    title.appendChild(help_text);
+    area.appendChild(title);
+  }
+  else
+  {
+    let list = document.createElement("ul");
+    list.setAttribute("class","mdc-list mdc-list--two-line");
+    let grid = document.createElement("div");
+    grid.setAttribute("class","mdc-layout-grid")
+    let grid_inner = document.createElement("div");
+    grid_inner.setAttribute("class", "mdc-layout-grid__inner");
+
+    grid.appendChild(grid_inner);
+    list.appendChild(grid);
+    area.appendChild(list);
+
+    for (let i = 0; i < projects_list.length; i++){
+      let project_name = document.createTextNode(projects_list[i].title);
+      let primary_text = document.createElement("span");
+      primary_text.setAttribute("class","mdc-list-item__primary-text");
+      primary_text.appendChild(project_name);
+
+      let due_date = document.createTextNode("Due Date: " + projects_list[i].due_date);
+      let secondary_text = document.createElement("span");
+      secondary_text.setAttribute("class","mdc-list-item__secondary-text");
+      secondary_text.appendChild(due_date);
+
+      let text = document.createElement("span");
+      text.setAttribute("class","mdc-list-item__text");
+      text.append(primary_text);
+      text.append(secondary_text);
+
+      let ripple = document.createElement("span");
+      ripple.setAttribute("class", "mdc-list-item__ripple")
+
+      let ripple_button = document.createElement("div");
+      ripple_button.setAttribute("class", "mdc-button__ripple")
+
+      let icon = document.createElement("i");
+      icon.setAttribute("class", "material-icons");
+      let del_icon = document.createTextNode("delete");
+      icon.appendChild(del_icon);
+
+      let button = document.createElement("button");
+      button.setAttribute("class", "mdc-button mdc-button--raised")
+      button.setAttribute("type", "button");
+      button.addEventListener("click", function(){
+        del_item(i);
+      });
+      button.appendChild(ripple_button);
+      button.appendChild(icon);
+
+      let lecturer = document.createElement("div");
+      lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
+      lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
+      lecturer.appendChild(button);
+
+      let link = document.createElement("a");
+      link.setAttribute("href", "details.html?project="+encodeURIComponent(projects_list[i].title+projects_list[i].unit));
+      link.setAttribute("style", "text-decoration:none;color: black;")
+      link.appendChild(text);
+
+      let new_item = document.createElement("li");
+      new_item.setAttribute("class","mdc-list-item")
+      new_item.appendChild(link);
+      new_item.appendChild(ripple);
+      new_item.appendChild(lecturer);
+
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "mdc-layout-grid__cell");
+      cell.setAttribute("id", "item"+i);
+      cell.appendChild(new_item);
+
+      grid_inner.appendChild(cell);
+    }
+  }
+}
   /*if(typeof(Storage) !== "undefined")
   {
     let projects = JSON.parse(localStorage.getItem("PROJECTS"));
