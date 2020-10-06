462daac2df7e5879af06f244688751dfef27291c
Dan <dyue0003@student.monash.edu>
2020-10-06 21:03:13 +0800

    Added alert before deleting a project, fixed delete visibility students
    Deleting a project now displays an alert 
    Delete button invisible to students on projects.html
    Changed text from "Hello" to "Loading" on details.html

--- src/public/js/projects.js
+++ src/public/js/projects.js
@@ -11,24 +11,6 @@ window.onload = function(){
       firestore.doc("students/" + id).get().then(function(doc){
         if (doc.data().status == "student")
         {
-          /*let student_projects = [];
-          for (let i = 0; i < doc.data().projects.length; i++)
-          {
-            student_projects.push(doc.data().projects[i]);
-          }
-          firestore.collection("projects").get().then(function(querySnapshot){
-            querySnapshot.forEach(function(project){
-              for (let j = 0; j < student_projects.length; j++)
-              {
-                if ((project.data().title + project.data().unit) == student_projects[j])
-                {
-                  projects_list.push(project.data());
-                }
-              }
-            })
-          }).then(function(){
-            generate_html();
-          });*/
           for (let i = 0; i < doc.data().projects.length; i++)
           {
             // Pushes every project student is involved into array
@@ -39,7 +21,7 @@ window.onload = function(){
               // all projects have been added
               if (i == (doc.data().projects.length-1))
               {
-                generate_html();
+                generate_html("0");
               }
             });
           }
@@ -52,7 +34,7 @@ window.onload = function(){
               projects_list.push(doc.data());
             })
           }).then(function(){
-            generate_html();
+            generate_html("1");
           });
         }
       });
@@ -64,13 +46,15 @@ window.onload = function(){
 }
 
 
-function generate_html()
+function generate_html(status)
 {
   if (projects_list.length == 0){
     let help_text = document.createTextNode("You have no active projects.");
     let title = document.createElement("h5");
     title.appendChild(help_text);
+    area.innerHTML += "<br>";
     area.appendChild(title);
+    area.innerHTML += "<br>";
   }
   else
   {
@@ -102,29 +86,36 @@ function generate_html()
       text.append(secondary_text);
 
       let ripple = document.createElement("span");
-      ripple.setAttribute("class", "mdc-list-item__ripple")
+      ripple.setAttribute("class", "mdc-list-item__ripple");
 
-      let ripple_button = document.createElement("div");
-      ripple_button.setAttribute("class", "mdc-button__ripple")
+      let div = document.createElement("div");
+      div.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
+      div.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
+      if (status == 1){
+        let ripple_button = document.createElement("div");
+        ripple_button.setAttribute("class", "mdc-button__ripple")
 
-      let icon = document.createElement("i");
-      icon.setAttribute("class", "material-icons");
-      let del_icon = document.createTextNode("delete");
-      icon.appendChild(del_icon);
+        let icon = document.createElement("i");
+        icon.setAttribute("class", "material-icons");
+        let del_icon = document.createTextNode("delete");
+        icon.appendChild(del_icon);
 
-      let button = document.createElement("button");
-      button.setAttribute("class", "mdc-button mdc-button--raised")
-      button.setAttribute("type", "button");
-      button.addEventListener("click", function(){
-        del_item(i);
-      });
-      button.appendChild(ripple_button);
-      button.appendChild(icon);
+        let button = document.createElement("button");
+        button.setAttribute("class", "mdc-button mdc-button--raised")
+        button.addEventListener("click", function(){
+          const warning = confirm("This project will be deleted permanently!\nAre you sure you want to delete this project?")
+          if (warning){
+            del_item(i)
+          }
+          else{
+            console.log("Not deleted")
+          }
+        });
+        button.appendChild(ripple_button);
+        button.appendChild(icon);
 
-      let lecturer = document.createElement("div");
-      lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
-      lecturer.innerHTML += "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;";
-      lecturer.appendChild(button);
+        div.appendChild(button);
+      }
 
       let link = document.createElement("a");
       link.setAttribute("href", "details.html?project="+encodeURIComponent(projects_list[i].title+projects_list[i].unit));
@@ -135,7 +126,7 @@ function generate_html()
       new_item.setAttribute("class","mdc-list-item")
       new_item.appendChild(link);
       new_item.appendChild(ripple);
-      new_item.appendChild(lecturer);
+      new_item.appendChild(div);
 
       let cell = document.createElement("div");
       cell.setAttribute("class", "mdc-layout-grid__cell");
