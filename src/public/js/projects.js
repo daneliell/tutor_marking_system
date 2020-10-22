e575560edb77a68c965dd3404745d4e3dfc917e7
Dan <dyue0003@student.monash.edu>
2020-10-23 01:18:10 +0800

    Changed HTML in projects.html
    Changed list of projects to list of cards containing information on each projects and team members involved. Fixed bug where students will not be able to see their own projects

--- src/public/js/projects.js
+++ src/public/js/projects.js
@@ -11,21 +11,31 @@ window.onload = function(){
       firestore.doc("students/" + id).get().then(function(doc){
         if (doc.data().status == "student")
         {
-          for (let i = 0; i < doc.data().projects.length; i++)
-          {
-            // Pushes every project student is involved into array
-            firestore.doc("projects/" + doc.data().projects[i]).get().then(function(project) {
-              if (project.data() != undefined) {
-                projects_list.push(project.data());
-              }
-            }).then(function(){
-              // If statement to only allow function to be called once after
-              // all projects have been added
-              if (i == (doc.data().projects.length-1))
-              {
-                generate_html("0");
-              }
-            });
+          if (doc.data().projects.length == 0){
+            let help_text = document.createTextNode("You have no active projects.");
+            let title = document.createElement("h5");
+            title.appendChild(help_text);
+            area.innerHTML += "<br>";
+            area.appendChild(title);
+            area.innerHTML += "<br>";
+          }
+          else{
+            for (let i = 0; i < doc.data().projects.length; i++)
+            {
+              // Pushes every project student is involved into array
+              firestore.doc("projects/" + doc.data().projects[i]).get().then(function(project) {
+                if (project.data() != undefined) {
+                  projects_list.push(project.data());
+                }
+              }).then(function(){
+                // If statement to only allow function to be called once after
+                // all projects have been added
+                if (i == (doc.data().projects.length-1))
+                {
+                  generate_html("0");
+                }
+              });
+            }
           }
         }
         else if (doc.data().status == "lecturer")
@@ -34,8 +44,8 @@ window.onload = function(){
             let help_text = document.createTextNode("You have no active projects.");
             let title = document.createElement("h5");
             title.appendChild(help_text);
-            area.innerHTML += "<br>";
             area.appendChild(title);
+            area.innerHTML += "<br>";
             area.innerHTML += "<br>";
           }
           else {
@@ -76,7 +86,7 @@ window.onload = function(){
   });
 }
 
-
+// Function to generate HTML using DOM
 function generate_html(status)
 {
   if (projects_list.length == 0){
@@ -89,7 +99,66 @@ function generate_html(status)
   }
   else
   {
-    let list = document.createElement("ul");
+    let grid = document.getElementById("area")
+    for (let i = 0; i < projects_list.length; i++){
+      let h2 = document.createElement("h2");
+      h2.setAttribute("class","mdl-card__title-text");
+      h2.innerHTML += projects_list[i].title
+
+      let title = document.createElement("div");
+      title.setAttribute("class", "mdl-card__title");
+      title.appendChild(h2);
+
+      let text = document.createElement("div");
+      text.setAttribute("class", "mdl-card__supporting-text");
+      text.innerHTML += "Due Date: " + projects_list[i].due_date + "<br>";
+      text.innerHTML += "Team Members: " + projects_list[i].members + "<br><br>";
+
+      let a = document.createElement("a");
+      a.setAttribute("class", "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect");
+      a.setAttribute("href", "details.html?project="+encodeURIComponent(projects_list[i].title+projects_list[i].unit))
+      a.innerHTML += "Details";
+
+      let border = document.createElement("div");
+      border.setAttribute("class", "mdl-card__actions mdl-card--border");
+      border.appendChild(a);
+
+      let i_elem = document.createElement("i");
+      i_elem.setAttribute("class", "material-icons")
+      i_elem.innerHTML = "delete";
+
+      let button = document.createElement("button")
+      button.setAttribute("class","mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect");
+      button.addEventListener("click", function(){
+        const warning = confirm("This project will be deleted permanently!\nAre you sure you want to delete this project?")
+        if (warning){
+          del_item(i)
+        }
+        else{
+          console.log("Not deleted")
+        }
+      });
+      button.appendChild(i_elem);
+
+      let menu = document.createElement("div");
+      menu.setAttribute("class","mdl-card__menu");
+      menu.appendChild(button);
+
+      let card = document.createElement("div");
+      card.setAttribute("class", "demo-card-wide mdl-card mdl-shadow--2dp");
+      card.appendChild(title);
+      card.appendChild(text);
+      card.appendChild(border);
+      card.appendChild(menu);
+
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "mdl-cell mdl-cell--4-col");
+      cell.appendChild(card);
+
+      grid.appendChild(cell);
+    }
+
+    /*let list = document.createElement("ul");
     list.setAttribute("class","mdc-list mdc-list--two-line");
     let grid = document.createElement("div");
     grid.setAttribute("class","mdc-layout-grid")
@@ -165,10 +234,11 @@ function generate_html(status)
       cell.appendChild(new_item);
 
       grid_inner.appendChild(cell);
-    }
+    }*/
   }
 }
 
+// Function to delete a project from the database
 function del_item(i){
   var firestore = firebase.firestore();
   let item = projects_list.splice(i,1); //[{project data}]
@@ -176,11 +246,12 @@ function del_item(i){
     const members = doc.data().members
     for (m in members){
       const studentRef = firestore.doc("students/" + members[m])
+      // Removes project from respective student
       studentRef.update({
         projects: firebase.firestore.FieldValue.arrayRemove(item[0].title + item[0].unit)
       })
     }
-
+    // Deletes entire project
     firestore.doc("projects/" + item[0].title + item[0].unit).delete().then(function() {
           document.location.reload();
       }).catch(function(error) {
